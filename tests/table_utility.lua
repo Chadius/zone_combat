e95d0f48324ce00bfeeae1af22fc78e2f637d0a7
Chad Serrant <therealchadius@gmail.com>
2019-04-28 18:13:06 -0400

    wip: Adding pluck function

--- tests/table_utility.lua
+++ tests/table_utility.lua
@@ -26,14 +26,89 @@ end
 
 function test_map()
   -- Test the map function to show it transforms the list.
+  local getStringLength = function(index, str, list)
+    return string.len(str)
+  end
+
+  local tab = {
+    "I",
+    "am",
+    "a",
+    "cat",
+  }
+
+  local map_table = TableUtility:map(tab, getStringLength)
+
+  assert_equal(4, TableUtility:size(map_table))
+  assert_equal(1, map_table[1])
+  assert_equal(2, map_table[2])
+  assert_equal(1, map_table[3])
+  assert_equal(3, map_table[4])
 end
 
 function test_filter()
   -- Test the filter function to show it can split the list.
+  local onlyEvenLength = function(index, str, list)
+    local str_len = string.len(str)
+    if str_len % 2 == 0 then
+      return true
+    end
+    return false
+  end
+
+  local tab = {
+    "I",
+    "am",
+    "a",
+    "cat",
+  }
+
+  local map_table = TableUtility:filter(tab, onlyEvenLength)
+
+  assert_equal(1, TableUtility:size(map_table))
+  assert_equal("am", map_table[1])
 end
 
 function test_pluck()
   -- Test the ability to select values based on a certain key from the table.
+  local tab = {
+    {
+      name="Larry",
+      dunks=10,
+      retired=true,
+    },
+    {
+      name="Michael",
+      dunks=50,
+      retired=true,
+    },
+    {
+      name="LeBron",
+      dunks=30,
+      retired=false,
+    },
+  }
+
+  local just_names = TableUtility:pluck(tab, "name")
+  assert_equal(3, TableUtility:size(just_names))
+  assert_equal("Larry", just_names[1])
+  assert_equal("Michael", just_names[2])
+  assert_equal("LeBron", just_names[3])
+
+  local just_dunks = TableUtility:pluck(tab, "dunks")
+  assert_equal(3, TableUtility:size(just_dunks))
+  assert_equal(10, just_dunks[1])
+  assert_equal(50, just_dunks[2])
+  assert_equal(30, just_dunks[3])
+
+  local just_retired = TableUtility:pluck(tab, "retired")
+  assert_equal(3, TableUtility:size(just_retired))
+  assert_equal(true, just_retired[1])
+  assert_equal(true, just_retired[2])
+  assert_equal(false, just_retired[3])
+
+  local just_bogus = TableUtility:pluck(tab, "bogus")
+  assert_equal(0, TableUtility:size(just_bogus))
 end
 
 function test_accumulate()

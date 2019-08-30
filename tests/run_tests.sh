0b4a392d4df0ac37bc0cf5ba5ae7f7b24965909c
Chad Serrant <therealchadius@gmail.com>
2019-08-30 08:24:56 -0400

    Moving table utility tests to a new folder.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-lua-5.1 ./tests/map.lua && lua-5.1 ./tests/tableUtility.lua && lua-5.1 ./tests/squaddieMovement.lua && \
+lua-5.1 ./tests/map.lua && lua-5.1 ./tests/utility/tableUtility.lua && lua-5.1 ./tests/squaddieMovement.lua && \
 lua-5.1 ./tests/squaddieTurn.lua && lua-5.1 ./tests/squaddieAffiliation.lua && lua-5.1 ./tests/territoryControl.lua && \
 lua-5.1 ./tests/squaddieHealthAndDeath.lua && lua-5.1 ./tests/action/ActionResolverTests.lua && lua-5.1 ./tests/mission/missionPhase.lua && \
 lua-5.1 ./tests/action/attackAction.lua
